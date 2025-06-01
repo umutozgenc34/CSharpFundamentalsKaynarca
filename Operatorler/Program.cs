1b4b01f646259b15d00fa3761abc162b145a1673
umutozgenc34 <umutozgenc34@hotmail.com>
2025-06-01 15:03:27 +0300

    basit uygulamalar

--- Operatorler/Program.cs
+++ Operatorler/Program.cs
@@ -70,5 +70,53 @@
 // 5. Bit Düzeyinde Operatörler -- Şimdi Değil --
 
 
+// UYGULAMALAR
 
+// ÖRNEK : x değerini y ye y değerini x e atayan algoritma (ek bir değişken oluşturarak)
+
+// int x = 5;
+// int y = 10;
+// int z = y; // z değişkeni y değerini tutar
+// y = x; // y değişkeni x değerini alır
+// x = z; // x değişkeni z değerini alır (y'nin eski değeri)
+
+// Console.WriteLine(x); -> output : 10
+// Console.WriteLine(y); -> output : 5
+
+
+// ÖRNEK: Karenin çevresini hesaplayan algoritma
+// Karenin çevresi, kenar uzunluğunun 4 katıdır.
+
+// Console.WriteLine("Karenin kenar uzunluğunu giriniz:");
+// int kenarUzunlugu = Convert.ToInt32(Console.ReadLine()); -> kullanıcıdan alına değer 5 varsayalım
+// int cevre = 4 * kenarUzunlugu; // Karenin çevresi 4 * kenar uzunluğu // output : 20
+// Console.WriteLine($"Karenin çevresi {cevre}"); -> output : Karenin çevresi 20
+
+// ÖRNEK: Dikdörtgenin çevresini hesaplayan algoritma
+// Dikdörtgenin çevresi (2 * (uzunluk + genişlik)) formülü ile hesaplanır.
+
+// Console.WriteLine("Dikdörtgenin uzun kenarını giriniz:");
+//int uzunKenar = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan alına değer 10 varsayalım
+// Console.WriteLine("Dikdörtgenin kısa kenarını giriniz:");
+// int kisaKenar = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan alına değer 5 varsayalım
+// int cevre = 2 * (uzunKenar + kisaKenar); // Dikdörtgenin çevresi 2 * (uzun kenar + kısa kenar) // output : 30
+// Console.WriteLine($"Dikdörtgenin çevresi {cevre}"); // output : Dikdörtgenin çevresi 30
+
+// ÖRNEK: Karenin alanını hesaplayan algoritma
+// Karenin alanı, kenar uzunluğunun karesidir.
+
+// Console.WriteLine("Kenar uzunluğunu giriniz");
+// int kenar = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan alına değer 5 varsayalım
+// int alan = kenar * kenar;  // output : 25
+// Console.WriteLine($"Karenin alanı {alan}"); // output : Karenin alanı 25
+
+// ÖRNEK: Dikdörtgenin alanını hesaplayan algoritma
+// Dikdörtgenin alanı (uzunluk * genişlik) formülü ile hesaplanır.
+
+// Console.WriteLine("Dikdörtgenin uzun kenarını giriniz");
+// int uzunKenar = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan alına değer 10 varsayalım
+// Console.WriteLine("Dikdörtgenin kısa kenarını giriniz");
+// int kisaKenar = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan alına değer 5 varsayalım
+// int alan = uzunKenar * kisaKenar; // Dikdörtgenin alanı (uzun kenar * kısa kenar) // output : 50
+// Console.WriteLine($"Dikdörtgenin alanı {alan}"); // output : Dikdörtgenin alanı 50
 
