135f258a0e6c1207693be059679a361ac91a5174
umutozgenc34 <umutozgenc34@hotmail.com>
2025-06-03 13:47:18 +0300

    debug (hata ayıklama)

--- KararYapilari/Program.cs
+++ KararYapilari/Program.cs
@@ -110,3 +110,56 @@
 // Yukarıdaki örnekte, kullanıcıdan sıcaklık değeri alınıyor ve bu değere göre giyinme önerileri yapılıyor.
 // Eğer sıcaklık 10 dereceden düşükse mont, 11-20 arası ise ceket, 21-35 arası ise tişört giyilmesi öneriliyor.
 // Eğer sıcaklık 35 dereceden yüksekse dışarı çıkmaması öneriliyor.
+
+
+// **** DEBUG (HATA AYIKLAMA) ****
+
+// Debugging (Hata Ayıklama), yazılım geliştirme sürecinde kodun çalışma zamanında nasıl davrandığını gözlemleyerek hataları bulmak ve düzeltmek için kullanılan bir tekniktir.
+// Visual Studio 2022, bu süreci oldukça kullanıcı dostu ve güçlü araçlarla desteklemektedir.
+
+// Temel debug araçları
+
+// 1. Breakpoint (Durma Noktası):
+// Breakpoint, kodun belirli bir satırında durmasını sağlar. Bu sayede o noktaya geldiğinde kodun durumunu inceleyebiliriz.
+// Breakpoint eklemek için kod satırının sol kenarına tıklayarak kırmızı bir nokta oluşturabilirsiniz.
+// Kodu çalıştırdığınızda, program bu noktada duracak ve değişkenlerin değerlerini inceleyebileceksiniz.
+// (Kod o satıra gelince çalışmayı durdurur ve geliştiriciye kontrol verir.)
+
+// 2.Start Debugging / Start Without Debugging
+// Start Debugging (F5) ile kodu debug modunda çalıştırabilirsiniz. Bu, breakpoint'lerinizi dikkate alarak kodu adım adım çalıştırmanızı sağlar.
+// Start Without Debugging (Ctrl + F5) ile kodu debug modunda olmadan çalıştırabilirsiniz. Bu, breakpoint'leri dikkate almaz ve kodu normal şekilde çalıştırır.
+
+// 3.Step Over (F10) / Step Into (F11) / Step Out (Shift + F11)
+
+// Step Over (F10): Bu seçenek, kodun bir sonraki satırına geçer.
+// Eğer o satırda bir metot çağrısı varsa, metot içindeki kodu atlar ve doğrudan metot çağrısının sonrasındaki satıra geçer.
+
+// F11 (Step Into): Fonksiyon çağrısına girer ve satır satır içeride devam eder.
+
+// Shift + F11 (Step Out): İçinde bulunduğunuz metottan çıkar ve çağrıldığı yere geri döner.
+
+// Örnek : 
+
+//int sayi = 10;
+//int ikinciSayi = 20;
+
+//if (sayi > ikinciSayi)
+//{
+//    Console.WriteLine("Birinci sayı daha büyük.");
+//}
+//else
+//{
+//    Console.WriteLine("İkinci sayı daha büyük veya eşit.");
+//}
+
+//Console.WriteLine("Program bitti.");
+
+//int sayi = 10; satırına breakpoint koyalım. o satırın olduğu sayının sol tarafına gelip tıklayarak kırmızı bir nokta oluşturalım.
+
+//F5 tuşu ile debug modunda çalıştıralım veya yukarıdan içi dolu yeşil ok simgesine tıklayalım.
+
+//F10 (Step Over) ile satır satır ilerleyerek değişkenlerin değerlerini gözlemleyelim.
+
+//if koşulunun nasıl çalıştığını izleyelim.
+
+//Console.WriteLine satırlarında kodun akışını inceleyelim.
