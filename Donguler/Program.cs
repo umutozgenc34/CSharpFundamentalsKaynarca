67dff3335d9950b591b0d9bd234c56a997b566ff
umutozgenc34 <umutozgenc34@hotmail.com>
2025-06-29 13:58:03 +0300

    foreach ve while döngüsü

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -83,3 +83,129 @@
 //}
 
 //Console.WriteLine("Dizinin elemanlarının toplamı: " + toplam);
+
+// ÖRNEK:
+// Kullanıcıdan 5 sayı alıp ortalmasını alma for döngüsü ile
+
+//int toplam = 0;
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    Console.WriteLine("Lütfen bir sayı giriniz.");
+//    int sayi = Convert.ToInt32(Console.ReadLine());
+//    toplam += sayi;
+//}
+
+//int ortalama = toplam / 5;
+//Console.WriteLine($"Sayıların ortalaması {ortalama}");
+
+
+// **** 2. foreach *****
+
+// foreach döngüsü, bir koleksiyonun (dizi, liste, vb.) elemanlarını tek tek dolaşmak için kullanılır.
+// Bu döngü, koleksiyonun her bir elemanını otomatik olarak alır ve belirli bir işlem yapar.
+// Söz dizimi şu şekildedir:
+// foreach (veri_tipi eleman in koleksiyon)
+// {
+//     // Döngü bloğu
+// }
+
+// ÖRNEK:
+// Bir dizinin elemanlarını foreach döngüsü ile yazdırma
+//string[] meyveler = { "Elma", "Armut", "Muz", "Çilek" };
+//foreach (string meyve in meyveler)
+//{
+//    Console.WriteLine(meyve);
+//}
+
+// ÖRNEK:
+// Bir dizinin elemanlarının toplamını foreach döngüsü ile hesaplama
+//int[] dizi = { 1, 2, 3, 4, 5 };
+//int toplam = 0;
+//foreach (int eleman in dizi)
+//{
+//    toplam += eleman;
+//}
+//Console.WriteLine("Dizinin elemanlarının toplamı: " + toplam);
+
+// ÖRNEK:
+// Rastgele sayıların olduğu dizi de pozitif , negatif ve sıfır sayıları ayırma
+// negatif ve pozitif sayıların toplamını hesaplama
+
+//int[] sayilar = { 1, -2, 3, 0, -5, 6, 0, -1 };
+//int negatifToplam = 0;
+//int pozitifToplam = 0;
+
+//foreach (int sayi in sayilar)
+//{
+//    if (sayi > 0)
+//    {
+//        Console.WriteLine($"{sayi} pozitif bir sayıdır.");
+//        pozitifToplam += sayi;
+//    }
+//    else if (sayi < 0)
+//    {
+//        Console.WriteLine($"{sayi} negatif bir sayıdır.");
+//        negatifToplam += sayi;
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{sayi} sıfırdır.");
+//    }
+
+//}
+
+//Console.WriteLine($"Negatif toplam : {negatifToplam}");
+//Console.WriteLine($"Pozitif toplam : {pozitifToplam}");
+
+// *** 3. while Döngüsü ***
+
+// while döngüsü, belirli bir koşul sağlandığı sürece kodun tekrar çalışmasını sağlar.
+// Bu döngüde koşul, döngü başlamadan önce kontrol edilir.
+
+// Söz dizimi şu şekildedir:
+
+// while (koşul)
+// {
+//     // Döngü bloğu
+// }
+
+// ÖRNEK:
+// 1'den 10'a kadar olan sayıları ekrana yazdıran bir while döngüsü
+//int i = 1;
+//while (i <= 10)
+//{
+//    Console.WriteLine(i);
+//    i++; // i'yi 1 artır
+//}
+
+// ÖRNEK:
+// 1'den 10'a kadar olan sayıların toplamını hesaplayan bir while döngüsü
+//int toplam = 0;
+//int i = 1;
+//while (i <= 10)
+//{
+//    toplam += i; // toplam = toplam + i;
+//    i++; // i'yi 1 artır
+//}
+
+//Console.WriteLine($"1'den 10'a kadar olan sayıların toplamı: {toplam}");
+
+// ÖRNEK:
+// Doğru şifre girilene kadar kullanıcıdan şifre alan döngü 
+
+//string dogruSifre = "1234";
+//string kullanicidanSifre = "";
+
+//while(dogruSifre != kullanicidanSifre)
+//{
+//    Console.WriteLine("Lütfen şifre giriniz");
+//    kullanicidanSifre = Console.ReadLine();
+
+//    if (dogruSifre!= kullanicidanSifre)
+//    {
+//        Console.WriteLine("Hatalı şifre tekrar deneyiniz");
+//    }
+//}
+
+//Console.WriteLine("Doğru şifre");
